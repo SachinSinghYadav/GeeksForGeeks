4c9e25fc6721f07ffd128fdf955015a62596119e
Sachin Singh Yadav <sachinsinghyadav245@gmail.com>
2020-08-29 15:03:17 +0530

    Save Ironman

---
+++ Array/palindrome.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore();
+    while(t--)
+    {
+        string s,s1,k;
+        getline(cin,s);
+        for(int i=0;s[i];i++)
+        {
+            if(s[i]!=' ')
+            {
+            if(s[i]>='A' && (int)s[i]<'Z')
+                s1.push_back((s[i]-'A')+97);
+            if((int)s[i]>='a' && (int)s[i]<='z')
+                s1.push_back(s[i]);
+            if(s[i]>='0' && s[i]<='9')
+                s1.push_back(s[i]);
+            }
+        }
+        s=s1;
+        reverse(s1.begin(),s1.end());
+        if(s1==s)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
